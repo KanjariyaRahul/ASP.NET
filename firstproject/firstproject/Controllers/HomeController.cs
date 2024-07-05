e2bedf0eae53504c8309252d7e3782707c17a35a
unknown <rahulkanjariya9265@gmail.com>
2024-07-05 08:15:06 +0530

    add project

---
+++ firstproject/firstproject/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using firstproject.Models;
+
+namespace firstproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Student s = new Student();
+            //s.Id = 1;
+            //s.Name = "rahul";
+            //s.PhoneNumber = 9265614292;
+
+            //string[] Name = {"rahul", "raj" , "jay" , "meet"};
+
+            List<Student> list = new List<Student>() {
+          new Student() {
+                    Id = 1,
+                    Name = "rahul",
+                    PhoneNumber = 9265614291
+            },
+            new Student() {
+                    Id = 2,
+                    Name = "jay",
+                    PhoneNumber = 1123365647
+            } };
+            ViewBag.model = list;
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ firstproject/firstproject/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace firstproject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+
+        public  Int64 PhoneNumber { get; set; }
+    }
+}
