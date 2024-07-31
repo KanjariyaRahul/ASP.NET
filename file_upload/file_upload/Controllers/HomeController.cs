82465261a74c9c03ee9ac13aa96f31c3d6ee5ef6
unknown <rahulkanjariya9265@gmail.com>
2024-07-31 12:54:00 +0530

    file upload chang

--- file_upload/file_upload/Controllers/HomeController.cs
+++ file_upload/file_upload/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.IO;
 namespace file_upload.Controllers
 {
     public class HomeController : Controller
     {
+
+        // Allowed file extensions
+        private static readonly string[] AllowedExtensions = { ".jpg", ".jpeg", ".png", ".gif" };
         public ActionResult Index()
         {
             ViewBag.Message = "Your application description page.";
@@ -15,12 +18,35 @@ namespace file_upload.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(HttpPostedFileBase f)
+        public ActionResult Index(HttpPostedFileBase file)
         {
-            string filename = f.FileName;
-            string newnm = Guid.NewGuid().ToString() + "_" + filename;
-            f.SaveAs(Server.MapPath("~") + newnm);
-            ViewBag.Message = "Your application description page.";
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName).ToLower();
+                if (Array.Exists(AllowedExtensions, ext => ext.Equals(extension)))
+                {
+                    try
+                    {
+                        string filename = Guid.NewGuid().ToString() + extension;
+                        string path = Path.Combine(Server.MapPath("~/Uploads/"), filename);
+                        file.SaveAs(path);
+                        ViewBag.ImagePath = Url.Content("~/Uploads/" + filename);
+                        ViewBag.Message = "File uploaded successfully!";
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Message = "Error occurred: " + ex.Message;
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid file type. Only JPEG, PNG, and GIF files are allowed.";
+                }
+            }
+            else
+            {
+                ViewBag.Message = "No file uploaded.";
+            }
             return View();
         }
         public ActionResult About()
